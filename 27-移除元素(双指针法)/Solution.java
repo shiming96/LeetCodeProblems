4b5af6134a237bf92ff2eea6ce2da0ac8323ecbf
ShiMing <mingshi_sm@163.com>
2019-03-21 09:19:25 +0800

    add 27

---
+++ 27-移除元素(双指针法)/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        int j = nums.length - 1;
+        while(i <= j) {
+            if(nums[i] == val) {
+                nums[i] = nums[j];
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return i;
+    }
+}
