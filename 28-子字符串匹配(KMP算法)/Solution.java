75f56189a5a5a349d605ac102851f7159fc74020
ShiMing <mingshi_sm@163.com>
2019-03-26 22:24:38 +0800

    update 28

--- 28-子字符串匹配(KMP算法)/Solution.java
+++ 28-子字符串匹配(KMP算法)/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
 
     //--------------------------------------------------
-    // 暴力查找  最坏情况下O(M+N)
+    // 暴力查找  最坏情况下O(M*N)
     //--------------------------------------------------
     public int strStr(String haystack, String needle) {
         if("".equals(needle) || needle == null)
@@ -21,4 +21,45 @@ class Solution {
         }
         return -1;
     }
+
+    //--------------------------------------------------
+    // KMP算法  O(M+N)
+    //--------------------------------------------------
+
+    public int strStr1(String haystack, String needle) {
+        int i = 0;
+        int j = 0;
+        int[] next = new int[needle.length() + 1];
+        getNext(needle, next);
+        while(i < haystack.length() && j < needle.length()) {
+            if(j == -1 || haystack.charAt(i) == needle.charAt(j)) {
+                ++i;
+                ++j;
+            } else
+                j = next[j];
+        }
+
+        if(j == needle.length())
+            return i - j;
+        return -1;
+
+    }
+
+
+    private void getNext(String p, int[] next) {
+        next[0] = -1;
+        int i = 0;
+        int j = -1;
+
+        while(i < p.length()) {
+            if(j == -1 || p.charAt(i) == p.charAt(j)) {
+                ++i;
+                ++j;
+                next[i] = j;
+            } else
+                j = next[j];
+        }
+
+    }
+
 }
