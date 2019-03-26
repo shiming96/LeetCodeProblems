b96194262e6aedeee32bc0e69fdb72bfe26c808b
ShiMing <mingshi_sm@163.com>
2019-03-26 22:24:00 +0800

    update 28

--- 28-子字符串匹配(KMP算法)/Solution.java
+++ 28-子字符串匹配(KMP算法)/Solution.java
@@ -1,24 +1,21 @@
 class Solution {
 
     //--------------------------------------------------
-    // 这是自己的解答，并不是KMP算法
+    // 暴力查找  最坏情况下O(M+N)
     //--------------------------------------------------
     public int strStr(String haystack, String needle) {
         if("".equals(needle) || needle == null)
             return 0;
         
-        char[] arr1 = haystack.toCharArray();
-        char[] arr2 = needle.toCharArray();
-        
         int j = 0;
         
-        for(int i = 0; i < arr1.length; i++) {
-            if(arr2[j] != arr1[i]) {
+        for(int i = 0; i < haystack.length(); i++) {
+            if(haystack.charAt(i) != needle.charAt(j)) {
                 i = i - j;
                 j = 0;
             } else {
                 j++;
-                if(j >= arr2.length)
+                if(j >= needle.length())
                     return i - j + 1;
             }
         }
